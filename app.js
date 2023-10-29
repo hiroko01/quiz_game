256af8b431b76b4bdee9a138ad37a60a6e3bb5c2
hiroko01 <hirokosakamoto31@gmail.com>
2023-10-29 16:46:36 +0900

    クイズゲームをつくる（基礎）が終わったところ

---
+++ app.js
@@ -0,0 +1,62 @@
+const question = 'ゲーム市場、最も売れたゲーム機は次のうちどれ？';
+const answers = [
+  'スーパーファミコン',
+  'プレイステーション',
+  'ニンテンドースイッチ',
+  'ニンテンドーDS'
+];
+const correct = 'ニンテンドーDS';
+
+// console.log(document.getElementById('js-question'));
+// console.log(document.getElementById('js-question').textContent);
+
+//定数の文字列をHTMLに反映させる
+const setupQuiz = () => {
+
+}
+
+document.getElementById('js-question').textContent = question;
+
+const $button = document.getElementsByTagName('button');
+
+// $button[0].textContent = answers[0];
+// $button[1].textContent = answers[1];
+// $button[2].textContent = answers[2];
+// $button[3].textContent = answers[3];
+
+let buttonIndex = 0;
+let buttonLength = $button.length;
+while(buttonIndex < buttonLength){
+  $button[buttonIndex].textContent = answers[buttonIndex];
+  buttonIndex++;
+}
+
+//ボタンをクリックしたら正誤判定
+$button[0].addEventListener('click', () => {
+   if(correct === $button[0].textContent){
+     window.alert('正解！');
+   } else {
+     window.alert('不正解！');
+   }
+});
+$button[1].addEventListener('click', () => {
+  if(correct === $button[1].textContent){
+    window.alert('正解！');
+  } else {
+    window.alert('不正解！');
+  }
+});
+$button[2].addEventListener('click', () => {
+  if(correct === $button[2].textContent){
+    window.alert('正解！');
+  } else {
+    window.alert('不正解！');
+  }
+});
+$button[3].addEventListener('click', () => {
+  if(correct === $button[3].textContent){
+    window.alert('正解！');
+  } else {
+    window.alert('不正解！');
+  }
+});
